a392aac08f1bacba3e2821c22c09e9425e6b0634
Isaac Naylor <i.acnaylor@gmail.com>
2025-07-13 23:50:44 +0100

    Added error on windows, we don't care about windows right now

--- src/main.zig
+++ src/main.zig
@@ -1,12 +1,21 @@
 const std = @import("std");
-const builtin = @import("builtin");
 const Allocator = std.mem.Allocator;
 const ArrayList = std.ArrayList;
+const builtin = @import("builtin");
+
+const Config = @import("Config.zig");
 
 pub fn main() !void {
+    if (builtin.os.tag == .windows) {
+        return error.UnsupportedOperatingSystem;
+    }
+
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer _ = gpa.deinit();
     const allocator = gpa.allocator();
+
+    const cfg = try Config.load(allocator);
+    defer cfg.deinit();
 }
 
 test {
