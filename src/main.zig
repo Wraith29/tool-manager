86ba87fdb9f37336deeda5afe2e464d3cbea9091
Isaac Naylor <i.acnaylor@gmail.com>
2025-07-13 23:10:44 +0100

    Removed filler code from main

--- src/main.zig
+++ src/main.zig
@@ -3,16 +3,10 @@ const builtin = @import("builtin");
 const Allocator = std.mem.Allocator;
 const ArrayList = std.ArrayList;
 
-const Hello = @import("cmd/Hello.zig");
-
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer _ = gpa.deinit();
     const allocator = gpa.allocator();
-
-    const cmd = Hello.command();
-    const hello_exe = try cmd.parse(allocator);
-    try hello_exe.execute(allocator);
 }
 
 test {
