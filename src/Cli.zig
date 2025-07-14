c85727b3ba8b6e61a2fef828e7dad43a59a6ed57
Isaac Naylor <i.acnaylor@gmail.com>
2025-07-15 00:17:05 +0100

    Added Cli struct

---
+++ src/Cli.zig
@@ -0,0 +1,52 @@
+const std = @import("std");
+const Allocator = std.mem.Allocator;
+const log = std.log.scoped(.cli);
+
+const Command = @import("cmd/Command.zig");
+const Executable = @import("cmd/Executable.zig");
+
+const Cli = @This();
+
+const commands = [_]Command{};
+
+const help =
+    \\tool-manager
+;
+
+allocator: Allocator,
+
+pub fn init(allocator: Allocator) Cli {
+    return Cli{ .allocator = allocator };
+}
+
+pub fn run(self: *const Cli) !void {
+    const args = try std.process.argsAlloc(self.allocator);
+    defer std.process.argsFree(self.allocator, args);
+
+    const exe: Executable = inline for (commands) |cmd| {
+        log.info("Checking {s}", .{cmd.name});
+        if (cmd.match(args[1])) {
+            if (std.mem.eql(u8, "--help", args[2]) or std.mem.eql(u8, "-h", args[2])) {
+                try std.io.getStdOut().writeAll(cmd.help);
+                std.process.cleanExit();
+            }
+
+            break cmd.parse(self.allocator, args[1..]) catch |err| switch (err) {
+                .MissingRequiredArgument => {
+                    try std.io.getStdErr().writeAll("Missing Required Argument\n");
+                    try std.io.getStdErr().writeAll(cmd.help);
+
+                    return err;
+                },
+                else => return err,
+            };
+        }
+    } orelse showHelp();
+
+    return exe.execute(self.allocator);
+}
+
+pub fn showHelp() noreturn {
+    try std.io.getStdOut().writeAll(showHelp);
+    std.process.cleanExit();
+}
