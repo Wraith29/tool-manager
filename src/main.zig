da6328892c9dcc52a290420a8dcb5881576aca3f
Isaac Naylor <i.acnaylor@gmail.com>
2025-07-14 09:00:12 +0100

    Added checks to ensure ~/.tool-manager exists

--- src/main.zig
+++ src/main.zig
@@ -2,8 +2,38 @@ const std = @import("std");
 const Allocator = std.mem.Allocator;
 const ArrayList = std.ArrayList;
 const builtin = @import("builtin");
+const log = std.log.scoped(.main);
 
 const Config = @import("Config.zig");
+const path = @import("path.zig");
+
+fn ensureToolPathsExist(cfg: *const Config) !void {
+    if (!path.exists(cfg.tool_path)) {
+        log.info("{s} not found, creating it", .{cfg.tool_path});
+        try std.fs.makeDirAbsolute(cfg.tool_path);
+    }
+
+    var tool_dir = try std.fs.openDirAbsolute(cfg.tool_path, .{});
+    defer tool_dir.close();
+
+    tool_dir.access("tools.json", .{}) catch {
+        log.info("tools.json not found, creating it", .{});
+        var tools_file = try tool_dir.createFile("tools.json", .{});
+        defer tools_file.close();
+
+        try tools_file.writeAll("{}");
+    };
+
+    tool_dir.access("src", .{}) catch {
+        log.info("src dir not found, creating it", .{});
+        try tool_dir.makeDir("src");
+    };
+
+    tool_dir.access("bin", .{}) catch {
+        log.info("bin dir not found, creating it", .{});
+        try tool_dir.makeDir("bin");
+    };
+}
 
 pub fn main() !void {
     if (builtin.os.tag == .windows) {
@@ -14,8 +44,10 @@ pub fn main() !void {
     defer _ = gpa.deinit();
     const allocator = gpa.allocator();
 
-    const cfg = try Config.init(allocator);
+    var cfg = try Config.init(allocator);
     defer cfg.deinit(allocator);
+
+    try ensureToolPathsExist(cfg);
 
     std.log.info("Tool Path: {s}", .{cfg.tool_path});
 }
