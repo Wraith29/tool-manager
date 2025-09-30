1e562bc4e455da23bd52b64c9f037c43af69eca4
Isaac Naylor <i.acnaylor@gmail.com>
2025-09-30 10:47:30 +0100

    Removed zig

--- src/string.zig
+++
@@ -1,9 +0,0 @@
-const std = @import("std");
-const Allocator = std.mem.Allocator;
-
-pub fn copy(allocator: Allocator, str: []const u8) Allocator.Error![]const u8 {
-    const mem = try allocator.alloc(u8, str.len);
-    @memcpy(mem, str);
-
-    return mem;
-}
